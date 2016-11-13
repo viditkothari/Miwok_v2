8fa67ca2350271cb0fd0f658a03b7cea7fc52e04
viditkothari <kothari.vidit@gmail.com>
2016-11-13 09:08:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/android/miwokv2/PhrasesActivity.java
@@ -0,0 +1,17 @@
+/*************************
+ * Copyright (c) 2016. This creation of Vidit Kothari (c) cannot be reproduced or modified or resell without prior written consent
+ ************************/
+
+package com.example.android.miwokv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhrasesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phrases);
+    }
+}
